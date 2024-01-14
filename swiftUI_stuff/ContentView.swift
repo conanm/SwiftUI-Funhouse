81fdcf6ad1698d2083c6b9791e8f02b544bc754a
cdm <conanm@gmail.com>
2024-01-14 11:31:13 +0100

    Initial Commit

---
+++ swiftUI_stuff/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUI_stuff
+//
+//  Created by Conan Moriarty on 14/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUI_stuff/swiftUI_stuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_stuffApp.swift
+//  swiftUI_stuff
+//
+//  Created by Conan Moriarty on 14/1/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_stuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
